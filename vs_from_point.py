613ec31da3837b07877fed2ac0237713fdaa08cd
RAIN-LX <rainy-v@outlook.com>
2024-09-13 07:21:57 -0500

    Create vs_from_point.py

---
+++ vs_from_point.py
@@ -0,0 +1,7 @@
+#setting up the viewshed tool allows you to copy what would have been a regular geoprocessing run 
+#into a python command
+
+#this is what the command looks like setup for my test environment
+with arcpy.EnvManager(outputCoordinateSystem='PROJCS["NAD_1983_UTM_Zone_16N",GEOGCS["GCS_North_American_1983",DATUM["D_North_American_1983",SPHEROID["GRS_1980",6378137.0,298.257222101]],PRIMEM["Greenwich",0.0],UNIT["Degree",0.0174532925199433]],PROJECTION["Transverse_Mercator"],PARAMETER["False_Easting",500000.0],PARAMETER["False_Northing",0.0],PARAMETER["Central_Meridian",-87.0],PARAMETER["Scale_Factor",0.9996],PARAMETER["Latitude_Of_Origin",0.0],UNIT["Meter",1.0]],VERTCS["NAD_1983",DATUM["D_North_American_1983",SPHEROID["GRS_1980",6378137.0,298.257222101]],PARAMETER["Vertical_Shift",0.0],PARAMETER["Direction",1.0],UNIT["Meter",1.0]]'):
+    out_raster = arcpy.sa.Viewshed("FL_GulfCoast_T_ProjectRaster", "ObserverPoints", 1, "FLAT_EARTH", 0.13, None); out_raster.save(r"S:\Viewshed\ViewshedAnalysisOUR\ViewshedAnalysisOUR.gdb\Viewshe_FL_G3"))
+
